1608dcf20afd7d1be4944ab4ea2b54d87b127139
hesham180899 <hesham.ashraf@meti.dev>
2025-01-01 16:31:38 +0200

    calculator commit

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+first_number =float(input("enter first number : "))   
+second_number =float(input("enter second number : "))  
+sum = first_number + second_number
+subtraction = first_number - second_number 
+multiplication = first_number * second_number 
+division = first_number / second_number
+print("Sum =",sum ,"\n Sub =",subtraction, "\n Multi =",multiplication, "\n Div =",division )
